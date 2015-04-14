d614e23c0934f4000fa3cd3502f628cdb888d57f
Olgierd Humenczuk <olgierdh@gmail.com>
2015-04-14 13:22:21 +0200

    [feat] added err printing to the debug scripts

--- src/debug.h
+++ src/debug.h
@@ -4,6 +4,7 @@
 #include <stdio.h>
 
 #define debug_printf( ... ) printf( __VA_ARGS__ )
+#define err_printf( ... ) fprintf( stderr, __VA_ARGS__ )
 
 #define debug_log( msg ) \
     debug_printf( "[%s@%d] - %s\r\n", __FILE__, __LINE__, msg ); \
@@ -12,5 +13,9 @@
 #define debug_fmt( fmt, ... ) \
     debug_printf( "[%s@%d] - " fmt "\r\n", __FILE__, __LINE__, __VA_ARGS__ ); \
     fflush( stdout )
+
+#define err_log( msg ) \
+    err_printf( "[%s@%d] - %s\r\n", __FILE__, __LINE__, msg ); \
+    fflush( stderr )
 
 #endif // __DEBUG_H__
